f3dd22f3459ef2a891f1080b2ac6b1b144699658
jlanej <jlanej@gmail.com>
2017-02-05 20:48:56 -0600

    :floppy_disk:

--- web/app.js
+++ web/app.js
@@ -1,36 +1,40 @@
 var initialLat = 40.7831;
 var initialLng = -73.9712;
 
-// var map = L.map('map').setView([initialLat, initialLng], 16);
+var map = L.map('map').setView([initialLat, initialLng], 16);
 //
-// // L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-// //     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-// //     subdomains: 'abcd',
-// //     // maxZoom: 19
-// // }).addTo(map);
-// //
-// // L.tileLayer('../test/{z}/{x}/{y}.png', {
-// //     // maxZoom: 19,
-// //     // opacity: 0.8
-// // }).addTo(map);
+L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    subdomains: 'abcd',
+    // maxZoom: 19
+}).addTo(map);
 
+// L.tileLayer('/test/{z}/{x}/{y}.png', {
+//     // maxZoom: 19,
+//     // opacity: 0.8
+// }).addTo(map);
 
-var osmTileJSON = {
-    "tilejson": "2.0.0",
-    "name": "OpenStreetMap",
-    "description": "A free editable map of the whole world.",
-    "version": "1.0.0",
-    "attribution": "&copy; OpenStreetMap contributors, CC-BY-SA",
-    "scheme": "xyz",
-    "tiles": [
-        "http://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
-        "http://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
-        "http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
-    ],
-    "minzoom": 0,
-    "maxzoom": 18,
-    "bounds": [ -180, -85, 180, 85 ],
-    "center": [ 11.9, 57.7, 8 ]
-};
-
-var map = L.tilejson.createMap('map', osmTileJSON);
+// $.getJSON("../out.json",function(data){
+//     // add GeoJSON layer to the map once the file is loaded
+//     L.geoJson(data).addTo(map);
+// });
+//
+// var osmTileJSON = {
+//     "tilejson": "2.0.0",
+//     "name": "OpenStreetMap",
+//     "description": "A free editable map of the whole world.",
+//     "version": "1.0.0",
+//     "attribution": "&copy; OpenStreetMap contributors, CC-BY-SA",
+//     "scheme": "xyz",
+//     "tiles": [
+//         "http://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
+//         "http://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
+//         "http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"
+//     ],
+//     "minzoom": 0,
+//     "maxzoom": 18,
+//     "bounds": [ -180, -85, 180, 85 ],
+//     "center": [ 11.9, 57.7, 8 ]
+// };
+//
+// var map = L.TileJSON.createMap('map', osmTileJSON);
