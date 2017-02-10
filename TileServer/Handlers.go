c8e3b10fda2521621159aa6fdadc17c5b1d890f7
jlanej <jlanej@gmail.com>
2017-02-09 22:29:32 -0600

    works and thats it

--- TileServer/Handlers.go
+++ TileServer/Handlers.go
@@ -20,7 +20,6 @@ func Tiles(w http.ResponseWriter, r *http.Request) {
 	z := vars["z"]
 	x := vars["x"]
 	y := vars["y"]
-	//fmt.Println(z +"\t"+x+"\t"+y)
 	db, err := sql.Open("sqlite3", "./tiles.mbtiles") //"./tiles.mbtiles")
 	if err != nil {
 		fmt.Print(err.Error())
@@ -38,13 +37,8 @@ func Tiles(w http.ResponseWriter, r *http.Request) {
 	}
 	var qrow = (1 << zoom_level) - 1 - rowNum
 	qrows := fmt.Sprint(qrow)
-	fmt.Println(qrows)
-	fmt.Println(qrow)
 
 	//dafuc 2
-
-	//(1 << zoom_level) - 1 - tile_row
-	//rows, err := db.Query("SELECT * FROM tiles WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?", z, x, y)
 	rows, err := db.Query("SELECT * FROM tiles WHERE zoom_level = ?  AND tile_column = ? AND tile_row = ?", z, x, qrows)
 	if err != nil {
 		fmt.Print(err.Error())
@@ -57,35 +51,19 @@ func Tiles(w http.ResponseWriter, r *http.Request) {
 		var tile_row int32
 		var tile_data []byte
 		rows.Scan(&zoom_level, &tile_column, &tile_row, &tile_data) //tile_data blob)
-		//roa, _ :=strconv.Atoi(y);
-		fmt.Println("HERE")
-		//
-		//fmt.Println(x)
-		//fmt.Println(tile_row-int32(roa))
-		////fmt.Println(tile_column)
-		//
-		//fmt.Println("THERE")
 
-		//if int32(roa)==tile_row {
-		//fmt.Println(string(tile_data))
-		//des suckers are gzipped sneaky buggers
 		var tile, erro = vector_tile.DecodeGzipped(tile_data)
 		if erro != nil {
 			fmt.Println(erro.Error())
 		}
-
-		fmt.Println(tile.GetLayers()[0].Features)
-		//fmt.Println(tile.GetLayers()[0].Values)
-
-		fmt.Println(zoom_level)
-		fmt.Println(tile_column)
-		fmt.Println(tile_row)
-
 		var unzi, _ = vector_tile.Encode(tile)
+		//fmt.Println("serving")
+		//fmt.Println(len(unzi))
+		//fmt.Println("bytes")
+
 		w.Write(unzi)
 		//}
 	}
 
 	db.Close()
-
 }
