87550b74a5bd6cb5b6c36047e0dfa05389bbf80b
Mr. Is <isaac.ardis@gmail.com>
2017-02-14 17:09:50 +0100

    flagging, formatting, and deplyiloying to [punktlich](punktlich.rotblauer.com)

--- TileServer/Handlers.go
+++ TileServer/Handlers.go
@@ -1,66 +1,17 @@
 package main
 
 import (
-	"database/sql"
 	"net/http"
 
 	"fmt"
 
 	"github.com/gorilla/mux"
-	_ "github.com/mattn/go-sqlite3"
 	// "github.com/paulmach/go.vector_tile"
 	//"strconv"
 	"encoding/binary"
 	"github.com/boltdb/bolt"
 	"strconv"
 )
-
-func Tiles(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Type", "application/x-protobuf")
-	w.Header().Set("Content-Encoding", "gzip") // set response header for encoding (browsers may do the ungzipping)
-	// https://github.com/SpatialServer/Leaflet.MapboxVectorTile/issues/29
-	vars := mux.Vars(r)
-	//dbname := vars["db"]
-	z := vars["z"]
-	x := vars["x"]
-	y := vars["y"]
-	db, err := sql.Open("sqlite3", "./tiles.mbtiles") //"./tiles.mbtiles")
-	if err != nil {
-		fmt.Print(err.Error())
-
-	}
-
-	//dafuc
-	zoom_level, e1 := strconv.ParseUint(z, 10, 32)
-	if e1 != nil {
-		fmt.Println(e1.Error())
-	}
-	rowNum, e2 := strconv.ParseUint(y, 10, 32)
-	if e2 != nil {
-		fmt.Println(e1.Error())
-	}
-	var qrow = (1 << zoom_level) - 1 - rowNum
-	qrows := fmt.Sprint(qrow)
-
-	//dafuc 2
-	rows, err := db.Query("SELECT * FROM tiles WHERE zoom_level = ?  AND tile_column = ? AND tile_row = ?", z, x, qrows)
-	if err != nil {
-		fmt.Print(err.Error())
-
-	}
-	for rows.Next() {
-
-		var zoom_level int32
-		var tile_column int32
-		var tile_row int32
-		var tile_data []byte
-		rows.Scan(&zoom_level, &tile_column, &tile_row, &tile_data) //tile_data blob)
-		w.Write(tile_data)
-	}
-
-	db.Close()
-}
 
 var (
 	db             *bolt.DB
@@ -166,24 +117,6 @@ func TilesBolty(w http.ResponseWriter, r *http.Request) {
 		return nil
 	})
 	if e != nil {
-		fmt.Println("getdb", e)
+		fmt.Println("getboltdb", e)
 	}
-
-	// //dafuc 2
-	// rows, err := db.Query("SELECT * FROM tiles WHERE zoom_level = ?  AND tile_column = ? AND tile_row = ?", z, x, qrows)
-	// if err != nil {
-	// 	fmt.Print(err.Error())
-
-	// }
-	// for rows.Next() {
-
-	// 	var zoom_level int32
-	// 	var tile_column int32
-	// 	var tile_row int32
-	// 	var tile_data []byte
-	// 	rows.Scan(&zoom_level, &tile_column, &tile_row, &tile_data) //tile_data blob)
-	// 	w.Write(tile_data)
-	// }
-
-	// db.Close()
 }
--- TileServer/Main.go
+++ TileServer/Main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"path"
 )
 
 func main() {
 
+	var mbtilesBoltDBPath string
+	var port string
+
+	flag.StringVar(&mbtilesBoltDBPath, "db", path.Join("./", "tiles.db"), "rel path of bolt db containing vector mbtiles file to serve")
+	flag.StringVar(&port, "port", "8080", "port to serve on")
+	flag.Parse()
+
 	router := NewRouter()
-	if bolterr := InitBoltDB("./../undump/tester.db"); bolterr == nil {
+
+	if bolterr := InitBoltDB(mbtilesBoltDBPath); bolterr == nil {
 		defer GetDB().Close()
 	} else {
 		log.Println(bolterr)
 	}
 
-	log.Println("Serving on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Println("Serving on :", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
 
 }
