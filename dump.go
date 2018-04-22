5375a41e16154feda44511e297ba4e690871433c
ia <isaac.ardis@gmail.com>
2018-04-22 09:38:08 +0900

    use absolute path for tippecanoe command

--- dump.go
+++ dump.go
@@ -100,7 +100,7 @@ func dumpBolty(boltDb string, out string) error {
 	//
 	//WARNINGS:
 	//Highest supported zoom with detail 14 is 18
-	tippCmd := "tippecanoe"
+	tippCmd := "/usr/local/bin/tippecanoe"
 	tippargs := []string{
 		"-ag",
 		"-M", "1000000",
@@ -118,7 +118,7 @@ func dumpBolty(boltDb string, out string) error {
 		"-o", out + ".mbtiles",
 	}
 	fmt.Println(">", tippCmd, tippargs)
-	tippmycanoe := exec.Command("tippecanoe", tippargs...)
+	tippmycanoe := exec.Command(tippCmd, tippargs...)
 	// tippmycanoe := exec.Command("tippecanoe", "-ag", "-pk", "-pf", "--drop-rate", "0", "--maximum-zoom", "50", "-g", "0.25", "-n", "catTrack", "-o", out+".mbtiles")
 	// tippmycanoe := exec.Command("tippecanoe", "-ag", "--full-detail", "20", "--low-detail", "14", "--minimum-detail", "8", "--maximum-tile-bytes", "1000000", "--maximum-zoom", "22", "-g", "1", "-n", "catTrack", "-o", out+".mbtiles")
 
