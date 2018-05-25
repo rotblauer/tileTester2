a2b6f0292273644a959678e4597a50ad9d5fed16
ia <isaac.ardis@gmail.com>
2018-05-26 00:57:46 +0200

    prob syntax getem? made refactor for funcyness

--- dump.go
+++ dump.go
@@ -36,6 +36,7 @@ func initBoltDB(boltDb string) error {
 
 	var err error
 	db, err = bolt.Open(boltDb, 0666, nil)
+
 	// db, err = bolt.Open(boltDb, 0666, &bolt.Options{ReadOnly: true})
 	// db, err = bolt.Open(boltDb, 0666, &bolt.Options{PageSize: os.Getpagesize() * 2})
 	// &bolt.Options{}
